915b416b773364aa6604515f9e8b5e8a0caf2819
Claire Dranginis <cdranginis@gmail.com>
2016-06-23 13:07:16 -0400

    started the solutions

--- models/tweet.js
+++ models/tweet.js
@@ -2,6 +2,43 @@ let db = require('./database');
 let Sequelize = require('sequelize');
 let User = require('./user');
 
-let Tweet = db.define('Tweet', {});
+let Tweet = db.define('Tweet', {
+  text: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 140]
+    }
+  },
+  dateCreated: {
+    type: Sequelize.DATE,
+    defaultValue: Date.now
+  }
+}, {
+  getterMethods: {
+    hashtags: function() {
+      var regex = /#\w+/gi;
+      return this.text.match(regex);  
+    }
+  }
+}, {
+  instanceMethods: {
+    timeWarp: function() {
+      return this.update({dateCreated: new Date('September 25, 1975')});
+    }
+  }
+}, {
+  classMethods: {
+    findByHashtag: function(hashtag) {
+      return Tweet.findAll({
+        where: {
+          text: {
+            $like: '%' + hashtag + '%'
+          }
+        }
+      });
+    }
+  }
+});
 
 module.exports = Tweet;
